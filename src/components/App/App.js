bae41ca44eca401ceda9d333a5979a60dc10894a
Maksim Vorobev <max11052003@yandex.ru>
2022-10-14 18:56:56 +0300

    ??

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,7 @@
 import '../../index.css';
 import './App.css';
 import React from 'react';
+import { useState } from 'react';
 import { Route, Routes } from 'react-router-dom'
 import { useNavigate } from 'react-router-dom';
 import MainScreen from '../MainScreen/MainScreen';
@@ -8,19 +9,40 @@ import SecondScreen from '../SecondScreen/SecondScreen';
 import ChooseCharacter from '../ChooseCharacter/ChooseCharacter';
 
 function App() {
+  const [currentCharacter, setCurrentCharacter] = useState(1)
   const navigate = useNavigate();
 
+ function changeCharacterState1(){
+    setCurrentCharacter(1)
+    document.getElementsByClassName(".buttonToChoose1").style.border = "border: 5px solid yellow;";
+    console.log("Залупа")
+ }
+ function changeCharacterState2(){
+    setCurrentCharacter(2)
+ }
+ function changeCharacterState3(){
+  setCurrentCharacter(3)
+ }
+ function nextPage(){
+  const gfgf = 4;
+  if(currentCharacter = 1) {navigate("/jija1")}
+  if(currentCharacter = 2) {navigate("/jija2")}
+  if(currentCharacter = 3) {navigate("/jija3")}
 
-  function GoNext(){
-    let path = `/start`
-    navigate(path)
-  }
-
+  // ? currentCharacter = 1 : navigate("/jija")
+  // ? currentCharacter = 2 : navigate("/jija2")  
+  // ? currentCharacter = 3 : navigate("/jija3");
+ }
   return (
     <Routes>
-      <Route path="/"exact element={<MainScreen/>} onClick={GoNext}></Route>
-      <Route path="/start" exact element={<SecondScreen/>}></Route>
-      <Route path="/choosecharacter" exact element={<ChooseCharacter/>}></Route>
+      <Route path="/"exact element={<MainScreen/>}/>
+      <Route path="/start" exact element={<SecondScreen/>}/>
+      <Route path="/choosecharacter" exact element={<ChooseCharacter/>} 
+      changeCharacterState1={changeCharacterState1} 
+      changeCharacterState2={changeCharacterState2} 
+      changeCharacterState3={changeCharacterState3}
+      nextPage={nextPage}
+      />
     </Routes>
   );
 }
--- src/components/ChooseCharacter/ChooseCharacter.js
+++ src/components/ChooseCharacter/ChooseCharacter.js
@@ -5,10 +5,19 @@ import res2 from '../../images/buttons/Resurs_2_4x.png'
 import { Link } from 'react-router-dom';
 
 
-function ChooseCharacter(){
+function ChooseCharacter(changeCharacterState2, changeCharacterState3, nextPage){
+
+    const changeCharacterState1 = () => {
+        document.getElementsById("12").style.border = "border: 5px solid yellow;";
+        console.log("Залупа")
+    }
+    
     return(
         <div className='App'>
         <div>
+            <button id='12' className='buttonToChoose1' onClick={changeCharacterState1}/>
+            <button className='buttonToChoose2' onClick={changeCharacterState2}/>
+            <button className='buttonToChoose3' onClick={changeCharacterState3}/>
             <img width="640" height="542" src={p2} alt="p2"></img>
             <a title='Следователь' target="_blank" rel='nooper noreferrer' href='https://drive.google.com/file/d/1Qqku69nm-I09zpUbsrNlUvIVsbru2P1m/view'>
                 <img alt='Следователь' src={res2} style={{position: 'absolute', width: '30px', top: '157px', left: 'calc(50% - 132px)'}}/>
@@ -17,8 +26,9 @@ function ChooseCharacter(){
                 <img className='criminal-case' alt='Следователь' src={res1}/>
             </a>
             <div className="Buttons">
-                <Link className='linkButton'>Дальше</Link>
+                <button className='linkButton' onClick={nextPage}>Дальше</button>
             </div>
+
         </div>
         </div>
     )
