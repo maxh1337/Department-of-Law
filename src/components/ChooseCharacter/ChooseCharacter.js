eff83575c7936b5e9071e42be1c5b09865c61a53
Maksim Vorobev <max11052003@yandex.ru>
2022-11-13 23:23:57 +0300

    Fixed chooseCharacter page

--- src/components/ChooseCharacter/ChooseCharacter.js
+++ src/components/ChooseCharacter/ChooseCharacter.js
@@ -1,64 +1,96 @@
-import p2 from '../../images/p2.jpg'
-import './ChooseCharacter.css'
-import res1 from '../../images/buttons/Resurs_1_4x.png'
-import res2 from '../../images/buttons/Resurs_2_4x.png'
-import { useState } from 'react'
-import { useNavigate } from 'react-router-dom';
-
+import p2 from "../../images/p2.jpg";
+import "./ChooseCharacter.css";
+import res1 from "../../images/buttons/Resurs_1_4x.png";
+import res2 from "../../images/buttons/Resurs_2_4x.png";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function ChooseCharacter() {
-    const navigate = useNavigate();
-    const [currentCharacter, setCurrentCharacter] = useState(0)
-    const [characterChosen, setCharacterChosen] = useState(0)
+  const navigate = useNavigate();
+  const [currentCharacter, setCurrentCharacter] = useState(0);
+  const [isChosen1, setIsChosen1] = useState(0);
+  const [isChosen2, setIsChosen2] = useState(0);
+  const [isChosen3, setIsChosen3] = useState(0);
 
-    const changeCharacterState = (e) => {
-        const btn = e.currentTarget
-        let chosenCharacter = btn.value
-
-        if (characterChosen === 0 || chosenCharacter === characterChosen){
-            if (btn.classList.contains("borderjs")) {
-                btn.classList.remove("borderjs")
-                chosenCharacter = 0;
-                setCharacterChosen(0)
-            } else {
-                btn.classList.add("borderjs")
-                setCharacterChosen(chosenCharacter)
-            }
-        } else {
-            console.log(chosenCharacter, characterChosen)
-        }
-        if (btn.value === "1") {
-            setCurrentCharacter(1)
-        } if (btn.value === "2") {
-            setCurrentCharacter(2)
-        } if (btn.value === "3") {
-            setCurrentCharacter(3)
-        }
+  const setChosen = (e) => {
+    if (e.target.value === "1") {
+      setIsChosen1(e.target.value);
+      setCurrentCharacter(e.target.value);
+      setIsChosen2(0);
+      setIsChosen3(0);
     }
+    if (e.target.value === "2") {
+      setIsChosen2(e.target.value);
+      setCurrentCharacter(e.target.value);
+      setIsChosen1(0);
+      setIsChosen3(0);
+    }
+    if (e.target.value === "3") {
+      setIsChosen3(e.target.value);
+      setCurrentCharacter(e.target.value);
+      setIsChosen1(0);
+      setIsChosen2(0);
+    }
+  };
 
-    const nextPage = () => {
-        if (currentCharacter === 1) { navigate("/choosecharacter1") }
-        if (currentCharacter === 2) { navigate("/choosecharacter1") }
-        if (currentCharacter === 3) { navigate("/choosecharacter1") }
-    } 
-
-    return (
-        <div>
-            <button value="1" id="test1" className='buttonToChoose1' onClick={changeCharacterState} />
-            <button value="2" id="test2" className='buttonToChoose2' onClick={changeCharacterState} />
-            <button value="3" id="test3" className='buttonToChoose3' onClick={changeCharacterState} />
-            <img width="640" height="542" src={p2} alt="p2"></img>
-            <a title='Следователь' target="_blank" rel='nooper noreferrer' href='https://drive.google.com/file/d/1Qqku69nm-I09zpUbsrNlUvIVsbru2P1m/view'>
-                <img alt='Следователь' src={res2} style={{ position: 'absolute', width: '30px', top: '157px', left: 'calc(50% - 132px)' }} />
-            </a>
-            <a title='Формы предварительного расследования' target="_blank" rel='nooper noreferrer' href='https://drive.google.com/file/d/1M1JpFqho86C8ZXcCcwUprGmtN0ILFSef/view'>
-                <img className='criminal-case' alt='Следователь' src={res1} />
-            </a>
-            <div className="Buttons">
-                <button className='linkButton' onClick={nextPage} id="btn1">Дальше</button>
-            </div>
-
-        </div>
-    )
+  return (
+    <div>
+      <button
+        value="1"
+        id="test1"
+        className={
+          isChosen1 === 0 ? "buttonToChoose1" : "buttonToChoose1 borderjs"
+        }
+        onClick={setChosen}
+      />
+      <button
+        value="2"
+        id="test2"
+        className={
+          isChosen2 === 0 ? "buttonToChoose2" : "buttonToChoose2 borderjs"
+        }
+        onClick={setChosen}
+      />
+      <button
+        value="3"
+        id="test3"
+        className={
+          isChosen3 === 0 ? "buttonToChoose3" : "buttonToChoose3 borderjs"
+        }
+        onClick={setChosen}
+      />
+      <img width="640" height="542" src={p2} alt="p2"></img>
+      <a
+        title="Следователь"
+        target="_blank"
+        rel="nooper noreferrer"
+        href="https://drive.google.com/file/d/1Qqku69nm-I09zpUbsrNlUvIVsbru2P1m/view"
+      >
+        <img
+          alt="Следователь"
+          src={res2}
+          style={{
+            position: "absolute",
+            width: "30px",
+            top: "157px",
+            left: "calc(50% - 132px)",
+          }}
+        />
+      </a>
+      <a
+        title="Формы предварительного расследования"
+        target="_blank"
+        rel="nooper noreferrer"
+        href="https://drive.google.com/file/d/1M1JpFqho86C8ZXcCcwUprGmtN0ILFSef/view"
+      >
+        <img className="criminal-case" alt="Следователь" src={res1} />
+      </a>
+      <div className="Buttons">
+        <button className="linkButton" onClick={() => {}} id="btn1">
+          Дальше
+        </button>
+      </div>
+    </div>
+  );
 }
 export default ChooseCharacter;
