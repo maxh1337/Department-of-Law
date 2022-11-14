7e4646f82f02e051122f781e0d1256acf48d51c2
Maksim Vorobev <max11052003@yandex.ru>
2022-11-14 18:17:32 +0300

    Fix: Styles and visual bugs at 31 page

--- src/components/30a/30a.js
+++ src/components/30a/30a.js
@@ -3,8 +3,10 @@ import { Navigate } from "react-router-dom";
 import p30а from "../../images/p30а.webp"
 import Criminal from "../ui/criminal-case/criminal-case"
 import Error from "../error/error";
+import styles from './30a.module.css'
 const step = JSON.parse(localStorage.getItem("step"));
 if (step !== 17) { localStorage.setItem("step", 17)};
+
 
 class ThirtyA extends React.Component{
     constructor(props){
@@ -37,17 +39,17 @@ class ThirtyA extends React.Component{
         return(
             (this.state.redirect === false) ?
             (this.state.error === false) ?
-            <div>
+            <div className={styles.mainDiv}>
                 <Criminal/>
-                <img width="640px" height="542px" src={p30а} alt="p30a"/>
-                <div data-multi-value="1" onClick={this.toggleMultiSet}  className={"multiple-cloud" + (this.state.multiSet.has(1) ? ' selected' : '')} style={{position: 'absolute', top: 1, left: 'calc(50% - 350px)', width: 172, height: 66}}>&nbsp;</div>
-                <div data-multi-value="2" onClick={this.toggleMultiSet}  className={"multiple-cloud" + (this.state.multiSet.has(2) ? ' selected' : '')} style={{position: 'absolute', top: 200, left: 'calc(50% - 350px)', width: 181, height: 75}}>&nbsp;</div>
-                <div data-multi-value="3" onClick={this.toggleMultiSet}  className={"multiple-cloud" + (this.state.multiSet.has(3) ? ' selected' : '')} style={{position: 'absolute', top: 350, left: 'calc(50% - 350px)', width: 200, height: 85}}>&nbsp;</div>
-                <div data-multi-value="4" onClick={this.toggleMultiSet}  className={"multiple-cloud" + (this.state.multiSet.has(4) ? ' selected' : '')} style={{position: 'absolute', top: 420, left: 'calc(50% - 350px)', width: 185, height: 75}}>&nbsp;</div>
-                <div onClick={() => {this.setState({error: true})}} className={"multiple-cloud"} style={{position: 'absolute', top: 24, left: 'calc(50% - -320px)', width: 180, height: 75}}>&nbsp;</div>
-                <div onClick={() => {this.setState({error: true})}} className={"multiple-cloud"} style={{position: 'absolute', top: 200, left: 'calc(50% - -320px)', width: 200, height: 85}}>&nbsp;</div>
-                <div onClick={() => {this.setState({error: true})}} className={"multiple-cloud"} style={{position: 'absolute', top: 330, left: 'calc(50% - -320px)', width: 185, height: 75}}>&nbsp;</div>
-                <div onClick={() => {this.setState({error: true})}} className={"multiple-cloud"} style={{position: 'absolute', top: 430, left: 'calc(50% - -320px)', width: 185, height: 75}}>&nbsp;</div>              
+                <img width="640px" height="542px" src={p30а}  alt="p30a"/>
+                <div data-multi-value="1" onClick={this.toggleMultiSet}  className={"multiple-cloud" + (this.state.multiSet.has(1) ? ' selected' : '')} style={{position: 'absolute', top: 1, left: 'calc(50% - 320px)', width: 50, height: 66 }}>&nbsp;</div>
+                <div data-multi-value="2" onClick={this.toggleMultiSet}  className={"multiple-cloud" + (this.state.multiSet.has(2) ? ' selected' : '')} style={{position: 'absolute', top: 210, left: 'calc(50% - 320px)', width: 50, height: 75}}>&nbsp;</div>
+                <div data-multi-value="3" onClick={this.toggleMultiSet}  className={"multiple-cloud" + (this.state.multiSet.has(3) ? ' selected' : '')} style={{position: 'absolute', top: 330, left: 'calc(50% - 320px)', width: 50, height: 85}}>&nbsp;</div>
+                <div data-multi-value="4" onClick={this.toggleMultiSet}  className={"multiple-cloud" + (this.state.multiSet.has(4) ? ' selected' : '')} style={{position: 'absolute', top: 430, left: 'calc(50% - 320px)', width: 50, height: 75}}>&nbsp;</div>
+                <div onClick={() => {this.setState({error: true})}} className={"multiple-cloud"} style={{position: 'absolute', top: 1, left: 'calc(50% - 0px)', width: 50, height: 75}}>&nbsp;</div>
+                <div onClick={() => {this.setState({error: true})}} className={"multiple-cloud"} style={{position: 'absolute', top: 210, left: 'calc(50% - 0px)', width: 50, height: 85}}>&nbsp;</div>
+                <div onClick={() => {this.setState({error: true})}} className={"multiple-cloud"} style={{position: 'absolute', top: 330, left: 'calc(50% - 0px)', width: 50, height: 75}}>&nbsp;</div>
+                <div onClick={() => {this.setState({error: true})}} className={"multiple-cloud"} style={{position: 'absolute', top: 430, left: 'calc(50% - 0px)', width: 50, height: 75}}>&nbsp;</div>             
             </div>
             : <Error text="Да... А протокол ты составлять так и не научился" button1="назад" button2style={{display: "none"}} onClick1={() => {this.setState({error: false})}}/>
             : <Navigate to="/31"/>
